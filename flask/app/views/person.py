cf1f7b270d39ed55c86681a99b1b28ed00e8ce41
Ng'etich Stephen <stephenngetich5@gmail.com>
2025-04-10 15:01:37 +0300

    feat: user can change names

--- flask/app/views/person.py
+++ flask/app/views/person.py
@@ -1,6 +1,11 @@
 from flask_restx import Namespace, Resource
-from app.helpers.response import get_success_response
+from flask import request
+from app.helpers.response import get_success_response, parse_request_body, validate_required_fields
 from app.helpers.decorators import login_required
+from common.app_config import config
+from common.services import AuthService, PersonService
+
+
 
 # Create the organization blueprint
 person_api = Namespace('person', description="Person-related APIs")
@@ -12,3 +17,23 @@ class Me(Resource):
     @login_required()
     def get(self, person):
         return get_success_response(person=person)
+
+@person_api.route('/change_name',
+                  doc=dict(description="Update Person first name and last name "))
+class UpdateName(Resource):
+    @person_api.expect(
+        {'type': 'object', 'properties': {
+            'entity_id': {'type': 'string'},
+            'first_name': {'type': 'string'},
+            'last_name': {'type': 'string'},
+        }}
+    )
+    @login_required()
+    def put(self):
+        parsed_body = parse_request_body(request, ['entity_id','first_name', 'last_name'])
+        validate_required_fields(parsed_body)
+
+        person_service = PersonService(config)
+
+        person_obj = person_service.update_person_name_by_id(parsed_body["entity_id"], parsed_body["first_name"], parsed_body["last_name"])
+        return get_success_response(person=person_obj)
